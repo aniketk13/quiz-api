41108b3e58caec0b7fbe6ff7911590c0a9afe3a5
Aniket Khajanchi <ani.khajanchi257@gmail.com>
2022-07-11 20:49:24 +0530

    changes done

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Question = require('./models/Question') //includes our model
 
-router.get('/', async (req, res) => {
+router.get('/', (req, res) => {
     return res.status(200).send("Welcome to the Quiz")
 })
 // get all quiz questions
